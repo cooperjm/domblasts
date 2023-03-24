4d965a6302c3eaff148aededda61e5630a12d198
cooperjm <cooper.jm@outlook.com>
2023-03-24 16:19:02 -0400

    Added Sidebar Toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'domblasts';
+  sideNavOpen = false;
+
+  @HostBinding('class.open') private get hostClass(): boolean {
+    return this.sideNavOpen;
+  }
+
+  toggleSideNav() {
+    this.sideNavOpen = !this.sideNavOpen;
+    console.log('Toggle Side Nav', this.sideNavOpen);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+const ROOT_ELEMENT_TAG = 'app-root';
+let rootElement = document.querySelector(ROOT_ELEMENT_TAG);
+
+if (!rootElement) {
+  rootElement = document.createElement(ROOT_ELEMENT_TAG);
+  rootElement.id = 'angular-chrome-app';
+  document.body.appendChild(document.createElement(ROOT_ELEMENT_TAG));
+}
+
 if (environment.production) {
   enableProdMode();
 }
